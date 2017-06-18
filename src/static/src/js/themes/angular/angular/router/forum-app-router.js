b63d5d9864fb6a0e720ddd588bbf0bff9328fed2
bitspur <bitspur.learning@gmail.com>
2017-06-18 16:01:04 +1000

    app student init

--- src/static/src/js/themes/angular/angular/router/forum-app-router.js
+++ src/static/src/js/themes/angular/angular/router/forum-app-router.js
@@ -11,25 +11,25 @@
 
 	app.config(function($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise('/website-pages/home');
-				$stateProvider.state('website-forum', {
+				$stateProvider.state('app-forum', {
 					abstract : true,
-					url : '/website-forum',
+					url : '/app-forum',
 					template : '<div ui-view class="ui-view-main" />'
-				}).state('website-forum.home', {
+				}).state('app-forum.home', {
 					url : '/home',
 					templateUrl : 'website/forum-home.html',
 					controller : [ '$scope','$rootScope', function($scope,$rootScope) {
 						$scope.app.settings.htmlClass = $rootScope.htmlClass.website;
 						$scope.app.settings.bodyClass = '';
 					} ]
-				}).state('website-forum.category', {
+				}).state('app-forum.category', {
 					url : '/category',
 					templateUrl : 'website/forum-category.html',
 					controller : [ '$scope','$rootScope', function($scope,$rootScope) {
 						$scope.app.settings.htmlClass = $rootScope.htmlClass.website;
 						$scope.app.settings.bodyClass = '';
 					} ]
-				}).state('website-forum.thread', {
+				}).state('app-forum.thread', {
 					url : '/thread',
 					templateUrl : 'website/forum-thread.html',
 					controller : [ '$scope','$rootScope', function($scope,$rootScope) {
